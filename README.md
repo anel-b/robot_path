208ddb95a175f705a7c82c62d9908c4bb1154cdd
anel-b <anybajrektarevic@gmail.com>
2024-08-25 12:15:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Robot Path Package in ROS 2 Humble for MoveIt 2
+
+### Package Contents of `my_robot_path`:
+
+**2 Nodes:**
+* **path_1.cpp**  
+   * Executes a path to a target pose while avoiding a point obstacle using the artificial potential field method.
+* **tutorial.cpp**  
+   * Executes a path to a target pose while avoiding a box obstacle.
---
+++ build/CMakeFiles/path_1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/anyba/ws_moveit2/src/my_robot_path/src/path_1.cpp" "CMakeFiles/path_1.dir/src/path_1.cpp.o" "gcc" "CMakeFiles/path_1.dir/src/path_1.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
